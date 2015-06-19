90756babc557dffbf8bc935abd4f61167075e677
Ruda Ottaviani <ruda.ottaviani@gmail.com>
2015-06-19 22:41:32 +0200

    Last Fix

--- testTidy.R
+++
@@ -1,4 +0,0 @@
-address <- "https://s3.amazonaws.com/coursera-uploads/user-4304538cdcb908807ca14fbe/973502/asst-3/75339f100fb611e5855d25210ba16071.txt"
-address <- sub("^https", "http", address)
-data <- read.table(url(address), header = TRUE)
-View(data)
